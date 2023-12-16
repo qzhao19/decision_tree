047b8e801cdf570b7450465885264dc5990732da
qzhao19 <qi.zhao@outlook.fr>
2023-12-16 23:43:15 +0800

    update

--- dtree/_splitter.py
+++ dtree/_splitter.py
@@ -26,7 +26,6 @@ class FeatureSplitter(object):
         self.end = end
 
         self.criterion._compute_node_histogram(y, self.samples, self.start, self.end)
-
         self.criterion._compute_node_impurity()
 
 
@@ -39,8 +38,28 @@ class FeatureSplitter(object):
                            improvement, 
                            missing_value):
         
+        # y is not constant (impurity > 0)
+        # has been checked by impurity stop criteria in build()
+        # moving on we can assume at least 2 samples
+
+        # Copy f_X=X[samples[start:end],f] training data X for the current node.
+        num_samples = self.end - self.start
+        f_x = np.zeros(num_samples)
+        for i in range(num_samples):
+            f_x[i] = X[samples[i] * self.num_features + feature_indice]
+
+        # Detect samples with missing values and 
+        # move them to the beginning of the samples vector
+        missing_value_indice = 0
+        for i in range(num_samples):
+            if np.isnan(f_x[i]):
+                f_x[i], f_x[missing_value_indice] = f_x[missing_value_indice], f_x[i]
+                samples[i], samples[missing_value_indice] = f_x[missing_value_indice], f_x[i]
+                missing_value_indice += 1
         
-
-
-
+        # Can not split feature when all values are NA
+        if missing_value_indice == num_samples:
+            return 
+        
+        
 
