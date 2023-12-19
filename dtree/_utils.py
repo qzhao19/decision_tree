27ebf4f514f790f6b8e2d76b3de65d8e1d188aaf
qzhao19 <qi.zhao@outlook.fr>
2023-12-19 23:11:01 +0800

    add utils.py

---
+++ dtree/_utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def sort(x, y, start, end, reverse=True):
+    x_y = [(x[i], y[i]) for i in range(len(x))]
+    
+    if not reverse:
+        x_y[start:end] = sorted(x_y[start:end], key=lambda z: z[0])
+    else:
+        x_y[start:end] = sorted(x_y[start:end], key=lambda z: z[0], reverse=True)
+    
+    x[:] = [z[0] for z in x_y]
+    y[:] = [z[1] for z in x_y]
+
+    return x, y
