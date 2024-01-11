8169239fc46e83775d49ee972d83b2f31e308f1c
qzhao19 <qi.zhao@outlook.fr>
2024-01-11 23:22:24 +0800

    update

--- dtree/_node.py
+++ dtree/_node.py
@@ -5,7 +5,7 @@ class Node(object):
         left_child, 
         right_child, 
         feature_indice, 
-        missing_value, 
+        has_missing_value, 
         threshold, 
         histogram, 
         impurity, 
@@ -13,7 +13,7 @@ class Node(object):
             self.left_child = left_child
             self.right_child = right_child
             self.feature_indice = feature_indice
-            self.missing_value = missing_value
+            self.has_missing_value = has_missing_value
             self.threshold = threshold
             self.histogram = histogram
             self.impurity = impurity
