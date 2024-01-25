da228ae41d4fc2f16c5b6f9153d14397fa48474a
qzhao19 <qi.zhao@outlook.fr>
2024-01-25 22:29:18 +0800

    update

--- dtree/_builder.py
+++ dtree/_builder.py
@@ -1,37 +1,119 @@
 import numpy as np
 
-from ._splitter import Splitter
+from ._splitter import EPSILON
 from ._tree import Tree
 
 
+class NodeInfo(object):
+    def __init__(self, start, end, depth, parent_indice, is_left):
+        self.start = start
+        self.end = end
+        self.depth = depth
+        self.parent_indice = parent_indice
+        self.is_left = is_left
+        
+
 class DepthFirstTreeBuilder(object):
+    """Build a binary decision tree in depth-first order.
+    """
     def __init__(self, 
+                 tree,
+                 splitter,
                  num_outputs, 
                  num_samples, 
                  num_features,
                  num_classes_max, 
                  num_classes_list,
+                 
+                 min_samples_split,
+                 min_samples_leaf,
+                 min_weight_leaf,
+
                  max_num_features,
                  max_num_thresholds,
                  max_depths,
                  class_weights
                 ):
+        self.tree = tree
+        self.splitter = splitter
         self.num_outputs = num_outputs
         self.num_samples = num_samples
         self.num_features = num_features
         self.num_classes_max = num_classes_max
         self.num_classes_list = num_classes_list
+
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_leaf = min_weight_leaf
+
         self.max_num_features = max_num_features
         self.max_num_thresholds = max_num_thresholds
         self.max_depths = max_depths
         self.class_weights = class_weights
 
 
-    def build(self, tree, X, y, num_samples):
+    def build(self, X, y, num_samples):
         
         # node information stack
-        # [start, end, depth, parent_indice, is_left]
-        node_info_stk = [(0, num_samples, 0, 0, False)]
-
+        # node_info_stk = [start, end, depth, parent_indice, is_left]
+        node_info_stk = []
+        node_info_stk.append(NodeInfo(0, num_samples, 0, 0, False))
         
+        while node_info_stk:
+            # Pop current node information from the stack
+            node_info = node_info_stk.pop()
+
+            # compute the number of samples in the current node
+            num_samples_node = node_info.end - node_info.start
+
+            # calculate number of samples per class histogram for all outputs
+            # and impurity for the current node
+            self.splitter.init_node(y, node_info.start, node_info.end)
+            histogram = self.splitter.get_weighted_histogram
+            impurity = self.splitter.get_impurity_node
+            partition_indice = 0
+
+            # stop criterion is met node becomes a leaf node
+            is_leaf = (node_info.depth >= self.max_depths or 
+                       num_samples_node < self.min_samples_split or 
+                       num_samples_node < 2 * self.min_samples_leaf or 
+                       num_samples_node < 2 * self.min_weight_leaf)
+            
+            if_leaf = is_leaf or impurity <= EPSILON
+
+            # split node if it is not leaf node
+            feature_indice = 0
+            has_missing_value = -1  # default: no missing value
+            threshold = None
+            improvement = 0.0
+
+            if not is_leaf:
+                split_info = self.splitter.split_node(X, y)
+                feature_indice = split_info["feature_indice"]
+                has_missing_value = split_info["has_missing_value"]
+                threshold = split_info["threshold"]
+                improvement = split_info["improvement"]
+                partition_indice = split_info["partition_indice"]
+
+                if split_info["improvement"] < EPSILON:
+                    if_leaf = True
+
+            node_indice = self.tree.add_node(
+                node_info.depth, 
+                node_info.parent_indice, 
+                node_info.is_left,
+                feature_indice, 
+                has_missing_value, 
+                threshold, 
+                histogram, 
+                impurity, 
+                improvement,
+            )
+
+            if not is_leaf:
+
+                node_info_stk.append(NodeInfo(0, num_samples, 0, 0, False))
+
+
+
 
