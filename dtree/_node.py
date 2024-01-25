9909e2afc15f9853be358f6b3e3d982fc06bd11c
qzhao19 <qi.zhao@outlook.fr>
2024-01-25 22:29:07 +0800

    update

--- dtree/_node.py
+++ dtree/_node.py
@@ -1,15 +1,44 @@
 import numpy as np
 
 class Node(object):
+    """Definition of a binary tree node
+    
+    Parameters:
+    ----------
+        left_child: int
+            left child indice 
+        
+        right_child: int
+            right child indice
+
+        feature_indices: int
+            the indice of feature chosen we want to split
+        
+        has_missing_value: int
+            if have the missing value if -1: No missing value, 
+            0: in the left child , 1: right child
+        
+        threshold: float
+            the threshold value decide how to split current feature 
+        
+        histogram: 2d array
+            weighted number of samples per class per output
+        
+        impurity: float
+        
+        improvement: float
+            for the feature importance
+
+    """
     def __init__(self, 
-        left_child, 
-        right_child, 
-        feature_indice, 
-        has_missing_value, 
-        threshold, 
-        histogram, 
-        impurity, 
-        improvement):
+                 left_child, 
+                 right_child, 
+                 feature_indice, 
+                 has_missing_value, 
+                 threshold, 
+                 histogram, 
+                 impurity, 
+                 improvement):
             self.left_child = left_child
             self.right_child = right_child
             self.feature_indice = feature_indice
