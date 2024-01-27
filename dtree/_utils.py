6ce0701d45ad0b4c1ce3936267a2bb28794375c8
qzhao19 <qi.zhao@outlook.fr>
2024-01-27 21:23:04 +0800

    update

--- dtree/_utils.py
+++ dtree/_utils.py
@@ -1,3 +1,4 @@
+import numbers
 import numpy as np
 
 def sort(x, y, start, end, reverse=True):
@@ -12,3 +13,36 @@ def sort(x, y, start, end, reverse=True):
     y[:] = [z[1] for z in x_y]
 
     return np.array(x), np.array(y)
+
+
+def check_random_state(seed):
+    """Turn seed into a np.random.RandomState instance.
+
+    Parameters
+    ----------
+    seed : None, int or instance of RandomState
+        If seed is None, return the RandomState singleton used by np.random.
+        If seed is an int, return a new RandomState instance seeded with seed.
+        If seed is already a RandomState instance, return it.
+        Otherwise raise ValueError.
+
+    Returns
+    -------
+    :class:`numpy:numpy.random.RandomState`
+        The random state object based on `seed` parameter.
+
+    """
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, numbers.Integral):
+        return np.random.RandomState(seed)
+    if isinstance(seed, np.random.RandomState):
+        return seed
+    raise ValueError(
+        "%r cannot be used to seed a numpy.random.RandomState instance" % seed
+    )
+
+
+
+
+
