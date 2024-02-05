d9f16998a98e63d1427ba67d8d10612f22a7713b
qzhao19 <qi.zhao@outlook.fr>
2024-02-05 23:05:36 +0800

    remove

--- test/run.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import test_criterion
-
-suite =unittest.TestSuite()
-
-suite.addTest(unittest.makeSuite(test_criterion.my_test))
-
-runner=unittest.TextTestRunner()
-runner.run(suite)  
