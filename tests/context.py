bbe9e4a8bbba8a1a3489cffa419c843fdb42cc17
qzhao19 <qi.zhao@outlook.fr>
2024-02-06 23:02:36 +0800

    add

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+if __package__:
+    from .. import dtree
+else:
+    # sys.path.append(os.dirname(__file__) + '/..')
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    import dtree
