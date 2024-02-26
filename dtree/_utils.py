76b7cb8089290081805b455b2a3d3231d92a12f9
qzhao19 <qi.zhao@outlook.fr>
2024-02-26 22:11:05 +0800

    update

--- dtree/_utils.py
+++ dtree/_utils.py
@@ -34,13 +34,14 @@ def check_random_state(seed):
     """
     if seed is None or seed is np.random:
         return np.random.mtrand._rand
-    if isinstance(seed, numbers.Integral):
+    elif isinstance(seed, numbers.Integral):
         return np.random.RandomState(seed)
-    if isinstance(seed, np.random.RandomState):
+    elif isinstance(seed, np.random.RandomState):
         return seed
-    raise ValueError(
-        "%r cannot be used to seed a numpy.random.RandomState instance" % seed
-    )
+    else:
+        raise ValueError(
+            "%r cannot be used to seed a numpy.random.RandomState instance" % seed
+        )
 
 
 def check_sample_weight(sample_weight, num_samples, dtype=None):
